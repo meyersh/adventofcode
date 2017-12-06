a87fcdc107b434ee62007c9225722e3b6e2ebd86
Shaun Meyer <meyersh@morningside.edu>
2017-12-05 20:29:04 -0600

    Vastly simplify pt 1 and complete pt2.

--- day2.hs
+++ day2.hs
@@ -1,6 +1,8 @@
+import Data.List
+
 -- Just like words, but with a string of integers instead of words.
 nums:: String -> [Int]
-nums s = map (\x->read x :: Int) $ words s
+nums s = sort $ map (\x->read x :: Int) $ words s
 
 minMax:: [Int] -> (Int, Int) -> (Int, Int)
 minMax (x:xs) (min, max)
@@ -11,16 +13,30 @@ minMax (x:xs) (min, max)
 minMax [] (min,max) = (min,max)
 
 minMax':: [Int] -> (Int, Int)
-minMax' (x:xs) = minMax (x:xs) (x,x)
+minMax' xs = (head xs, last xs)
 
 diffMinMax:: (Int,Int) -> Int
 diffMinMax (a,b) = abs (a-b)
 
+pairWise':: Int -> [Int] -> [(Int,Int)]
+pairWise' a (x:xs) = (a,x):(pairWise' a xs)
+pairWise' a _      = []
+
+pairWise:: [Int] -> [(Int,Int)]
+pairWise (x:xs) = (pairWise' x xs)++(pairWise xs)
+pairWise []     = []
+
 main =
   do
     fileData <- readFile "inputs/day2.input"
-    putStrLn "Part One"
+    putStrLn "Part One:"
     putStrLn $ show $ sum $ map (diffMinMax . minMax') (map nums $ lines fileData) -- array of [Int]'s
+
+    putStrLn "part Two:"
+    -- 1. Make a matrix of [[row1], [row2], ...]
+    -- 2. Map each [rowN]
+    -- 2.1 Filter each [rowN] for pairs that don't divide.
+    putStrLn $ show $ sum $ (map ((\(x,y) -> (quot y x)) . head . filter (\(x,y) -> rem y x == 0) . pairWise) (map nums $ lines fileData))
  --   return $ show numbers
 
 --return $ show $ diffMinMax $ minMax row (head row, last row)
