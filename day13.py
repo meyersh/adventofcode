ad52e8d9906e62c2fc33c4eb2dfd4f71858672d5
Shaun Meyer <meyersh@morningside.edu>
2018-12-13 23:56:43 -0600

    Part A of day 13 good, Part B...
     - Leaving in several attempts at the repair() function. Finally
       settled on removing the carts from the grid and calling it a
       track[y][x], we always will reset to that in the future.
    
     - Part b is not working, but I'm not sure. This thing has been
       running rockstar on example inputs.

---
+++ day13.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+import copy
+
+with open("inputs/day13.txt") as input:
+    gridy = [list(line.rstrip()) for line in input.readlines()]
+    track = copy.deepcopy(gridy)
+
+# Make the track by removing all carts (assume carts aren't on
+# intersections.)
+for y in range(0,len(track)):
+    for x in range(0,len(track[y])):
+        if track[y][x] in "<>":
+            track[y][x] = '-'
+        if track[y][x] in "v^":
+            track[y][x] = '|'
+            
+class Cart():
+    def __init__(self, x, y):
+        self.turns = 0
+        self.x = x
+        self.y = y
+        self.active = True
+
+    def __repr__(self):
+        return "<Cart: %d turns @ (%d,%d) (Active:%s)>" % (
+            self.turns, self.x, self.y, str(self.active))
+
+def prettyprint(grid=gridy):        
+    for y in range(0,len(gridy)):
+        for x in range(0,len(gridy[y])):
+            print(grid[y][x], end="")
+
+        print()
+
+cartshapes = ('^', '>', 'v', '<')
+
+def findcarts(gridy=gridy):
+    carts = list()
+    for y in range(0,len(gridy)):
+        for x in range(0,len(gridy[y])):
+            if gridy[y][x] in cartshapes:
+                carts.append((x,y))
+
+    return carts
+
+carts = [Cart(cart[0], cart[1]) for cart in findcarts()]
+
+pprint(carts)
+
+def left(c):
+    return cartshapes[(cartshapes.index(c) - 1) % 4]
+
+def right(c):
+    return cartshapes[(cartshapes.index(c) + 1) % 4]
+
+"""
+horizontal piece repairs WHEN:
+
+1: ---
+2: \-/
+3: +-+
+4: \-\
+5: /-/
+6: /-\
+
+vertical piece repairs when:
+
+|  +  \
+|  |  |
+|  +  /  ...
+
+
+
+"""
+def repair(x,y):
+    global track
+    return track[y][x]
+
+def repairb(x,y):
+    global gridy
+    # Look around x,y and interpolate the correct track.
+    try:
+        if (gridy[y][x-1] in ('-', '+') and gridy[y][x+1] in ('-', '+')
+            and gridy[y-1][x] in ('|', '+', '\\', '/') and gridy[y+1][x] in ('|', '+', '\\', '/')):
+            return '+'
+    except: pass # not an intersection.
+
+    try:
+        if (gridy[y][x-1] in ('-', '\\', '/', '+')
+            and gridy[y][x+1] in ('-', '\\', '/', '+')):
+            return '-'
+    except: pass # not a horizontal.
+
+    try:
+        if (gridy[y-1][x] in ('|', '\\', '/', '+')
+            and gridy[y+1][x] in ('|', '\\', '/', '+')):
+            return '|'
+    except: pass # not a vertical.
+
+    try:
+        if gridy[y][x+1] in ('-', '+') and gridy[y-1][x] in ('|', '+'):
+            return '\\'
+    except: pass # not Bottom left
+    
+    try:
+        if gridy[y][x-1] in ('-', '+') and gridy[y-1][x] in ('|', '+'):
+            return '/'
+    except: pass # not Bottom right
+
+    try:
+        if gridy[y][x+1] in ('-', '+') and gridy[y+1][x] in ('|', '+'):
+            return '/'
+    except: pass # not Top left
+        
+    try:
+        if gridy[y][x-1] in ('-', '+') and gridy[y+1][x] in ('|', '+'):
+            return '\\'
+    except: pass # not Top right.
+
+    # Well... What about the last grid?
+    print("Trying to place in from lastgrid -> '%s'" % lastgrid[y][x])
+    return lastgrid[y][x]
+
+def repaira(x,y):
+    global gridy, lastgrid
+    # Look around x,y and interpolate the correct track.
+    try:
+        if (gridy[y][x-1] in ('-', '+') and gridy[y][x+1] in ('-', '+')
+            and gridy[y-1][x] in ('|', '+', '\\', '/') and gridy[y+1][x] in ('|', '+', '\\', '/')):
+            return '+'
+    except:
+        pass # not an intersection.
+
+    try:
+        if gridy[y][x-1] in ('-', '/', '\\') and gridy[y][x+1] in ('-', '/', '\\'):
+            return '-'
+    except: pass
+
+    try:
+        if gridy[y-1][x] == gridy[y+1][x]:
+            return '|'
+    except: pass
+
+    # Bottom left
+    if gridy[y][x+1] in ('-', '+') and gridy[y-1][x]  in ('|', '+'):
+        return '\\'
+
+    # Bottom right
+    if gridy[y][x-1] in ('-', '+')  and gridy[y+1][x] in ('|', '+'):
+        return '/'
+
+    # Top left
+    if gridy[y][x+1] in ('-', '+') and gridy[y+1][x] in ('|', '+'):
+        return '/'
+    
+    # Top right.
+    if gridy[y][x-1] in ('-', '+') and gridy[y+1][x] in ('|', '+'):
+        return '\\'
+
+
+def tick(gridy=gridy, verbose=False, stoponcrash=True):
+
+    for cart in sorted(carts, key=lambda cart: (cart.y, cart.x)):
+        if not cart.active:
+            continue
+        if verbose: print("Moving cart %s." % str((cart.x, cart.y)))
+        c = gridy[cart.y][cart.x] # directionish.
+
+        # Get the next coordinate.
+        if c == '>':
+            nextx = cart.x + 1
+            nexty = cart.y
+        elif c == '<':
+            nextx = cart.x - 1
+            nexty = cart.y
+        elif c == '^':
+            nextx = cart.x
+            nexty = cart.y - 1
+        elif c == 'v':
+            nextx = cart.x
+            nexty = cart.y + 1
+        else:
+            print("Struggling, looking at c '%s' in %s " % (c, cart))
+
+        # Handle intersections
+        if gridy[nexty][nextx] == '+':
+            # We turn left, straight, right, over and over.
+            if cart.turns % 3 == 0:
+                c = left(c)
+            elif cart.turns % 3 == 1:
+                c = c # nothing changes, go straight.
+            elif cart.turns % 3 == 2:
+                c = right(c)
+
+            cart.turns += 1
+            
+
+        # Handle curves
+        if gridy[nexty][nextx] == '\\':
+            if c in ('>', '<'):
+                c = right(c)
+            else:
+                c= left(c)
+
+        if gridy[nexty][nextx] == '/':
+            if c in ('<', '>'):
+                c = left(c)
+            else:
+                c = right(c)
+
+
+        # Handle straight line bits.
+        if gridy[nexty][nextx] in ('-', '|'):
+            pass
+
+        if gridy[nexty][nextx] in cartshapes:
+            if stoponcrash:
+                gridy[nexty][nextx] = "X"
+                print("TERRIBLE CRASH: %s" % str((nextx,nexty)))
+                return False
+            else:
+                gridy[nexty][nextx] = repair(nextx, nexty)
+                gridy[cart.y][cart.x] = repair(cart.x, cart.y)
+                cart.active = False
+                for othercart in carts:
+                    if othercart.x == nextx and othercart.y == nexty:
+                        othercart.active = False
+                        
+
+        # Otherwise, move the cart and repair the carts previous
+        # location with new track.
+
+        # Omit this to leave streamers...
+        gridy[cart.y][cart.x] = repair(cart.x,cart.y)
+        assert gridy[cart.y][cart.x] is not None
+        
+        cart.x = nextx
+        cart.y = nexty
+
+        gridy[nexty][nextx] = c
+
+    return True
+
+
+n=0
+
+
+###
+### PARTY TIME
+###
+
+### PART 1
+
+#while tick():
+#    n+=1
+#    prettyprint()
+#    print("tock..%d"%n)
+#    print(sorted(carts, key=lambda cart: (cart.y, cart.x)))
+#    print("TURNS: %s" % " ".join(["{:04d}".format(cart.turns) for cart in carts]))
+
+
+# NOT 19,136.
+# NOT 93,80...
+# NOT 127,117
+# NOT 104,30
+# NOT 103,30
+
+### PART 2
+
+
+while len(carts)> 1:
+    n+=1
+    print("tock..%d"%n)
+    print(sorted(carts, key=lambda cart: (cart.y, cart.x)))
+    print("TURNS: %s" % " ".join(["{:04d}".format(cart.turns) for cart in carts]))
+    carts = [c for c in carts if c.active]
+    tick(stoponcrash=False)
+# NOT 118,104
+# NOT 117,103
+# NOT 119,104...
+print(carts)
